e80342829266b1fed242f1995d3eb44b73558bf0
manjila <manjila2059@gmail.com>
2025-11-16 10:17:15 +0545

    main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,14 +6,9 @@ import { Sidebar } from "../components/sidebar/sidebar";
 export default function MainLayout() {
   return (
     <>
-<<<<<<< HEAD
-      <Sidebar navLinks={navLinks} />
-      <main className=" md:ml-64 ">
-=======
       <Sidebar />
 
       <main className="min-h-screen ml-64 p-6 bg-gray-50">
->>>>>>> d5758e992ea224115e72dddad97631307314d975
         <Outlet />
       </main>
 
