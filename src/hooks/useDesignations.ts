5a6a70579020aeedb3d38c8c07e8f0fe48c9c632
Sushila Neupane <168651501+sushilaneupane@users.noreply.github.com>
2025-11-23 16:33:36 +0545

    Update useDesignations.ts

--- src/hooks/useDesignations.ts
+++ src/hooks/useDesignations.ts
@@ -1,32 +1,74 @@
-import { useQuery ,useMutation} from "@tanstack/react-query";
-import{getDesignations,Designation, getDesignationsByDepartment,AddDesignationData,addDesignation} from "../api/designationApi"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getDesignations,
+  getDesignationsByDepartment,
+  addDesignation,
+  deleteDesignation,
+  updateDesignation,
+} from "../api/designationApi";
+import {
+  Designation,
+  AddDesignationData,
+  UpdateDesignationData,
+} from "@/types/designation";
 
 
 export const useDesignations = (departmentId: string) => {
   return useQuery<Designation[], Error>({
-    queryKey: ["designations", departmentId], 
-    queryFn: () => getDesignations(departmentId), 
-    enabled: !!departmentId, 
+    queryKey: ["designations", departmentId],
+    queryFn: () => getDesignations(departmentId),
+    enabled: !!departmentId,
   });
 };
 
-export const useDesignationsByDepartment = ( departmentId: string) => {
+export const useDesignationsByDepartment = (departmentId: string) => {
   return useQuery<Designation[], Error>({
     queryKey: ["designations", departmentId],
-    queryFn: () => getDesignationsByDepartment( departmentId),
-    enabled:  !!departmentId,
+    queryFn: () => getDesignationsByDepartment(departmentId),
+    enabled: !!departmentId,
   });
 };
 
 export const useAddDesignation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (data: AddDesignationData) => addDesignation(data),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["designations"] });
+    },
   });
 };
 
-export const useDeleteDesignation = ()=>{
-  
-}
-export const useUpdateDesignation = ()=>{
-  
-}
+
+export const useDeleteDesignation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: string) => deleteDesignation(id),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["designations"] });
+    },
+  });
+};
+
+
+export const useUpdateDesignation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      id,
+      data,
+    }: {
+      id: string;
+      data: UpdateDesignationData;
+    }) => updateDesignation(id, data),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["designations"] });
+    },
+  });
+};
