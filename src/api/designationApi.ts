cdf0fed08d84c6f7f6f747f31f2c6ba3f3c7671f
Sushila Neupane <168651501+sushilaneupane@users.noreply.github.com>
2025-11-23 16:31:00 +0545

    Update designationApi.ts

--- src/api/designationApi.ts
+++ src/api/designationApi.ts
@@ -1,20 +1,6 @@
+import { AddDesignationData, DeleteDesignationResponse, Designation, UpdateDesignationData } from "@/types/designation";
 import { axiosInstance } from "../api/axiosInstance";
 
-export interface Designation {
-  designationId: string;
-  designationName: string;
-  departmentId: string;
-  
-  isActive: boolean;
-}
-export interface AddDesignationData{
-   departmentId: string;
-  designationName: string;
-
-}
-export interface DeleteDesignationResponse {
-  success: boolean;
-}
 export const getDesignations = async (departmentId: string) => {
   const { data } = await axiosInstance.get(`/Designation`, {
     params: { departmentId }, 
@@ -22,19 +8,25 @@ export const getDesignations = async (departmentId: string) => {
   return data as Designation[];
 };
 
-
 export const getDesignationsByDepartment = async ( departmentId: string) => {
- const res = await axiosInstance.get(`/Designation/${departmentId}/Department`);
+ const res = await axiosInstance.get(`/designation/?DepartmentId:${departmentId}`);
   return res.data.data as Designation[];
 };
 export const addDesignation = async (data: AddDesignationData) => {
-  const response = await axiosInstance.post(`/Designation`, data);
+  const response = await axiosInstance.post(`/designation`, data);
   return response.data;
 };
 
 export const deleteDesignation = async (id: string): Promise<DeleteDesignationResponse> => {
-  const response = await axiosInstance.delete(`/Designation/${id}`);
+  const response = await axiosInstance.delete(`/designation/${id}`);
   return response.data;
+  
 };
 
-
+export const updateDesignation = async (
+  id: string,
+  data: UpdateDesignationData
+) => {
+  const response = await axiosInstance.patch(`/designation/${id}`, data);
+  return response.data;
+};
